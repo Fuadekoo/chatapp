25d439cb709efbc8a5ec81895ebbc76ed8ed8de6
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-05-24 11:03:50 +0300

    Refactor Profile component layout and enhance Drawer overlay styling

--- app/[lang]/(user)/chat/profile.tsx
+++ app/[lang]/(user)/chat/profile.tsx
@@ -1,9 +1,33 @@
+import Image from "next/image";
 import React from "react";
 
 function Profile() {
   return (
-    <div>
-      <h1>This is the profile page</h1>
+    <div className="grid grid-rows-[1fr_auto] overflow-hidden">
+      <div className="items-center justify-center flex flex-col gap-4 p-4">
+        <Image
+          src="/ai.png"
+          alt="Profile"
+          width={100}
+          height={100}
+          className=" object-cover border"
+        />
+
+        <button>change profile picture</button>
+        <hr className="w-full border-t" />
+      </div>
+      <div className="flex flex-col justify-center p-4">
+        <h1>about me</h1>
+        <ul>
+          <li>
+            <strong>Name:</strong> John Doe
+          </li>
+          <li>
+            <strong>Email:</strong> email@example.com
+          </li>
+        </ul>
+        <button className="mt-4 p-4">Logout</button>
+      </div>
     </div>
   );
 }
--- components/drawer.tsx
+++ components/drawer.tsx
@@ -11,7 +11,7 @@ export default function Drawer({ open, onClose, children }: DrawerProps) {
     <>
       {/* Overlay */}
       <div
-        className={`fixed inset-0 bg-black bg-opacity-30 z-40 transition-opacity duration-300 ${
+        className={`fixed inset-0 bg-blur bg-opacity-30 backdrop-blur-xs z-40 transition-opacity duration-300 ${
           open
             ? "opacity-100 pointer-events-auto"
             : "opacity-0 pointer-events-none"
